24a8925c93f67c39c73dd5164657c0fbe5ddec0d
Madiator2011 <romanrex19@int.pl>
2024-06-16 13:37:34 +0200

    Add files via upload

---
+++ container-template/start.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -e  # Exit the script if any statement returns a non-true return value
+
+# ---------------------------------------------------------------------------- #
+#                          Function Definitions                                #
+# ---------------------------------------------------------------------------- #
+
+# Start nginx service
+start_nginx() {
+    echo "Starting Nginx service..."
+    service nginx start
+}
+
+# Execute script if exists
+execute_script() {
+    local script_path=$1
+    local script_msg=$2
+    if [[ -f ${script_path} ]]; then
+        echo "${script_msg}"
+        bash ${script_path}
+    fi
+}
+
+# Setup SSH
+setup_ssh() {
+    if [[ $PUBLIC_KEY ]]; then
+        echo "Setting up SSH..."
+        mkdir -p ~/.ssh
+        echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
+        chmod 700 -R ~/.ssh
+
+        for key_type in rsa dsa ecdsa ed25519; do
+            key_path="/etc/ssh/ssh_host_${key_type}_key"
+            if [ ! -f ${key_path} ]; then
+                ssh-keygen -t ${key_type} -f ${key_path} -q -N ''
+                echo "${key_type^^} key fingerprint:"
+                ssh-keygen -lf ${key_path}.pub
+            fi
+        done
+
+        service ssh start
+
+        echo "SSH host keys:"
+        for key in /etc/ssh/*.pub; do
+            echo "Key: $key"
+            ssh-keygen -lf $key
+        done
+    fi
+}
+
+# Export environment variables
+export_env_vars() {
+    echo "Exporting environment variables..."
+    printenv | grep -E '^RUNPOD_|^PATH=|^_=' | awk -F = '{ print "export " $1 "=\"" $2 "\"" }' >> /etc/rp_environment
+    echo 'source /etc/rp_environment' >> ~/.bashrc
+}
+
+# Start code-server
+start_code_server() {
+    echo "Starting code-server..."
+    nohup code-server --bind-addr 0.0.0.0:7777 --auth none --disable-telemetry /workspace &> /code-server.log &
+    echo "code-server started"
+}
+
+# ---------------------------------------------------------------------------- #
+#                               Main Program                                   #
+# ---------------------------------------------------------------------------- #
+
+start_nginx
+
+execute_script "/pre_start.sh" "Running pre-start script..."
+
+echo "Pod Started"
+
+setup_ssh
+start_code_server
+export_env_vars
+
+execute_script "/post_start.sh" "Running post-start script..."
+
+echo "Start script(s) finished, pod is ready to use."
+
+sleep infinity
---
+++ helper-templates/verify-nccl/Dockerfile
@@ -0,0 +1,21 @@
+FROM runpod/pytorch:2.0.1-py3.10-cuda11.8.0-devel
+
+ENV NCCL_DEBUG=TRACE
+
+WORKDIR /verify-nccl
+
+# Install additional packages
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    nano \
+    pciutils \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN git clone https://github.com/NVIDIA/cuda-samples.git && \
+    cd cuda-samples/Samples/0_Introduction/simpleP2P && \
+    make
+
+COPY check_nccl.sh .
+RUN chmod +x check_nccl.sh
+
+# Start Container
+CMD tail -f /dev/null
